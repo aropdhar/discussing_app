dca9c7bc2d2dafb43d07411c6f7c8188249b4974
Arop Sutra Dhar <142885013+aropdhar@users.noreply.github.com>
2024-05-08 14:56:48 +0600

    fromik_add

--- src/component/layout/utilities/Inputbox.jsx
+++ src/component/layout/utilities/Inputbox.jsx
@@ -1,10 +1,10 @@
 import { TextField } from '@mui/material'
 import React from 'react'
 
-const Inputbox = ({variant , placeholder}) => {
+const Inputbox = ({variant , placeholder , id , name , onChange ,value , type}) => {
   return (
     <>
-         <TextField fullWidth id="standard-basic" label={placeholder} variant={variant} />
+         <TextField fullWidth id={id} type={type} name={name} onChange={onChange} label={placeholder} variant={variant} value={value} />
     </>
   )
 }
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -11,7 +11,8 @@ import Image from '../../component/layout/utilities/Image';
 import Inputbox from '../../component/layout/utilities/Inputbox';
 import { NavLink } from 'react-router-dom';
 import loginbg from '../../assets/Rectangle 69.webp'
-
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -26,6 +27,7 @@ const Loginhead = styled(Typography)({
   lineHeight: '45.41px',
   color: "#03014C",
   fontSize: '33.34px',
+  marginBottom: '30px'
 })
 
 const BootstrapButton = styled(Button)({
@@ -37,31 +39,81 @@ const BootstrapButton = styled(Button)({
   lineHeight: 1.5,
   backgroundColor: '#5F34F5',
   borderColor: '#0063cc',
-  width: '424.9px'
+  width: '424.9px',
+  marginBottom: '30px'
 });
 
 
 const Login = () => {
+   
+  let initialValues = {
+    email: '',
+    password: ''
+  }
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: Yup.object({
+      password: Yup.string()
+        .max(10, 'Must be 10 characters or less')
+        .min(5 , 'must be 5 characters')
+        .required('Please Enter Your Password'),
+      email: Yup.string()
+      .email('Invalid email address')
+      .required('Please Enter Your Email'),
+    }),
+    onSubmit: values => {
+       console.log(values);
+      // alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
 
           <Grid container spacing={0}>
             <Grid item xs={6} style={{display: "flex" , alignItems: "center" , justifyContent: "center"}}>
-              <div className='login_main'>
+              <div>
                   <Loginhead variant="h4">
                       Login to your account!
                   </Loginhead>
                   <Image source={googleimg} alt="Not Found" styledimg="googleimg"/>
-                  
-                  <Inputbox variant="standard" placeholder="Email Address"/>
+                <form onSubmit={formik.handleSubmit}>
+                  <div className='login_main'>
+                      <Inputbox 
+                       type="email" 
+                       id="email"
+                       variant="standard" 
+                       placeholder="Email Address"
+                       onChange={formik.handleChange}
+                       value={formik.values.email}
+                       />
+                          {formik.touched.email && formik.errors.email ? (
+                            <div className='fromikerror'>{formik.errors.email}</div>
+                          ) : null}
+                      <Inputbox 
+                       type='password'
+                       id='password'
+                       variant="standard"
+                       placeholder="Enter Password"
+                       onChange={formik.handleChange}
+                       value={formik.values.password}
+                       />
+                      
+                      {formik.touched.password && formik.errors.password ? (
+                            <div className='fromikerror'>{formik.errors.password}</div>
+                      ) : null}
 
-                  <Inputbox variant="standard" placeholder="Enter Password"/>
 
-                  <BootstrapButton variant="contained" disableRipple>
-                    Login to Continue
-                  </BootstrapButton>
-                  <span style={{fontSize: "13.34px" , color: '#03014C' , fontweight:"700", lineHeight: "18.16px" }}>Don’t have an account ? <NavLink to='/registration' style={{fontSize: "13.34px" , color: 'red' , fontweight:"700", lineHeight: "18.16px" }}>Sign up</NavLink></span>
+                      <BootstrapButton type='submit' variant="contained" disableRipple>
+                        Login to Continue
+                      </BootstrapButton>
+                      
+                  </div>
+                </form>
+
+                   
+                  <span style={{fontSize: "13.34px" , color: '#03014C' , fontweight:"700",lineHeight: "18.16px" }}>Don’t have an account ? <NavLink to='/registration' style={{fontSize: "13.34px" , color: 'red' , fontweight:"700", lineHeight: "18.16px" }}>Sign up</NavLink></span>
               </div>
             </Grid>
 
