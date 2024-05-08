0ccfba4eb89675ff1aba096786071544d3227610
Arop Sutra Dhar <142885013+aropdhar@users.noreply.github.com>
2024-05-08 12:54:42 +0600

    update

--- src/component/userlist/Userlist.jsx
+++ src/component/userlist/Userlist.jsx
@@ -5,9 +5,36 @@ import Heading from '../heading/Heading'
 const Userlist = () => {
   return (
     <>
-      <div className='userlist_main'>
+       <div className='userlist_main'>
          <Heading text="User List"/>
+         <div className='item_main'>
+            <div className='item'>
+
+            </div>
+            <div className='item'>
+
+            </div>
+            <div className='item'>
+
+            </div>
+            <div className='item'>
+
+            </div>
+            <div className='item'>
+
+            </div>
+            <div className='item'>
+
+            </div>
+            <div className='item'>
+
+            </div>
+            <div className='item'>
+
+            </div>
+         </div>
       </div>
+
     </>
   )
 }
